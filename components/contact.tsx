d903b84d7d527ea84f6257d66b7472d019f5c79e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-15 00:28:18 +0530

    updated

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,7 +32,7 @@ export default function Contact() {
     >
       <SectionHeading>Contact me</SectionHeading>
 
-      <p className="text-gray-700 -mt-6 ">
+      <p className="text-gray-700 -mt-6  dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:example@gmail.com">
           example@gmail.com
