5f0d84cbe895d52fa94b326f57ef5a74fb05fe1b
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-15 00:19:25 +0530

    experience updated

--- components/experience.tsx
+++ components/experience.tsx
@@ -7,10 +7,13 @@ import 'react-vertical-timeline-component/style.min.css';
 import { useSectionInView } from "@/lib/hooks";
 import { experiencesData } from '@/lib/data';
 import './experience.scss';
+import { useTheme } from "@/context/theme-context";
 
 export default function Experience() {
 
   const { ref } = useSectionInView("Experience");
+  const { theme } = useTheme();
+
 
   return (
     <section id='experience' ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
@@ -20,7 +23,8 @@ export default function Experience() {
                     <React.Fragment  key={index}>
                         <VerticalTimelineElement
                            contentStyle={{
-                            background: "#f3f4f6",
+                            background:
+                              theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                             boxShadow: "none",
                             border: "1px solid rgba(0, 0, 0, 0.05)",
                             textAlign: "left",
@@ -28,20 +32,24 @@ export default function Experience() {
                             padding: "1.3rem 2rem",
                           }}
                           contentArrowStyle={{
-                            borderRight: "0.4rem solid #9ca3af",
+                            borderRight:
+                              theme === "light"
+                                ? "0.4rem solid #9ca3af"
+                                : "0.4rem solid rgba(255, 255, 255, 0.5)",
 
                           }}
                           date={item.date}
                           icon={item.icon}
                           iconStyle={{
-                            background: "white",
+                            background:
+                            theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                             fontSize: "1.5rem",
                           }}
 
                         >
                             <h3  className="font-semibold capitalize mb-2">{item.title}</h3>
                             <p className="font-normal !mt-0  mb-2">{item.location}</p>
-                            <p className="!mt-1 !font-normal text-gray-700  mb-2">{item.description}</p>
+                            <p className="!mt-1 !font-normal text-gray-700  mb-2 dark:text-white/75">{item.description}</p>
                         </ VerticalTimelineElement>
                     </React.Fragment>
                 ))}    
