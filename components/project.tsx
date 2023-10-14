223a1c1bd18212736b17bca8534b790fc4f8500a
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-15 00:12:20 +0530

    updated

--- components/project.tsx
+++ components/project.tsx
@@ -31,14 +31,14 @@ export default function Project({ title, description, tags, imageUrl}:
         <section 
             className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
             mb-3 sm:mb-8 last:mb-0 even-pl-8 hover:bg-gray-200 transition
-            group-even:pl-8'>
+            group-even:pl-8 dark:text-white'>
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[20rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
                 <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
                     {tags.map((tag, index) => (
                         <li 
-                            className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full'
+                            className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full  dark:text-white/70'
                             key={index}
                         >{tag}
                         </li>
