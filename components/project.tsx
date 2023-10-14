fde228cb0c9a7e52725d97abdb219fb359b11167
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-15 00:12:39 +0530

    updated

--- components/project.tsx
+++ components/project.tsx
@@ -31,7 +31,7 @@ export default function Project({ title, description, tags, imageUrl}:
         <section 
             className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
             mb-3 sm:mb-8 last:mb-0 even-pl-8 hover:bg-gray-200 transition
-            group-even:pl-8 dark:text-white'>
+            group-even:pl-8 dark:text-white dark:bg-white/10'>
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[20rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
