b66c90e6a2aa9a12f5b0a77ea5e627b95f0521da
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-16 20:37:32 +0530

    updated

---
+++ components/smoothCorners.tsx
@@ -0,0 +1,28 @@
+// "use client"
+
+// import React, { ReactNode, CSSProperties } from 'react';
+
+// interface SmoothCornersProps {
+//   children: ReactNode;
+//   borderRadius?: string;
+//     corners?: string;
+//   style?: CSSProperties;
+// }
+
+
+// const SmoothCorners: React.FC<SmoothCornersProps> = ({ 
+//     children, 
+    
+//     borderRadius = '10px', 
+//     corners="30px",
+//     style = {} 
+// }) => {
+//     const combinedCorners = corners ? { borderRadius: corners } : { borderRadius };
+//     return (
+//         <div style={{ ...combinedCorners, ...style }}>
+//             {children}
+//         </div>
+//     );
+// };
+
+// export default SmoothCorners;
