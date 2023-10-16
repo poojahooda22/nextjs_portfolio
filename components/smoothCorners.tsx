12fcaab82fddb8642075beafbff664b4dd829f81
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-16 20:42:33 +0530

    updated

--- components/smoothCorners.tsx
+++ components/smoothCorners.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { ReactNode, CSSProperties } from 'react';
+// import React, { ReactNode, CSSProperties } from 'react';
 
 interface SmoothCornersProps {
   children: ReactNode;
