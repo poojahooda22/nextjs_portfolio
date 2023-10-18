da1341c5ebba4df1c0f00170db7fe1f5bc755fab
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-18 23:08:48 +0530

    updated

--- components/contact.tsx
+++ components/contact.tsx
@@ -79,7 +79,8 @@ export default function Contact() {
           Submit 
           <FaPaperPlane 
             className="text-xs opacity-70 transition-all group-hover:translate-x-1 
-            group-hover:-translate-y-1"  /> {" "}
+            group-hover:-translate-y-1"  
+            /> {" "}
         </button>
       </form>
     </motion.section>
