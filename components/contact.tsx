06a2d4af066b8534807a8ad6480344339c247cff
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-18 23:07:39 +0530

    updated

--- components/contact.tsx
+++ components/contact.tsx
@@ -76,7 +76,7 @@ export default function Contact() {
           className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] 
           bg-gray-900 text-white rounded-full outline-none transition-all dark:bg-[#1d2432]"
         >
-            Submit 
+          Submit 
             <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"  /> {" "}
         </button>
       </form>
