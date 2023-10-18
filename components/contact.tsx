a7fbe22620d340bc4f3e0a17a22da96d39445313
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-18 23:08:05 +0530

    updated

--- components/contact.tsx
+++ components/contact.tsx
@@ -77,7 +77,8 @@ export default function Contact() {
           bg-gray-900 text-white rounded-full outline-none transition-all dark:bg-[#1d2432]"
         >
           Submit 
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"  /> {" "}
+          <FaPaperPlane 
+          className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"  /> {" "}
         </button>
       </form>
     </motion.section>
