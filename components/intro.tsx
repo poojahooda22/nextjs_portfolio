91a59e515600ea8c99dc659498d8b89dffad2cf1
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-19 21:55:03 +0530

    updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -40,7 +40,8 @@ export default function Intro() {
                         height="192"
                         quality="95"
                         priority={true}
-                        className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+                        className='h-24 w-24 rounded-full object-cover border-[0.35rem] 
+                        border-white shadow-xl'
                     />
                 </motion.div>
                 <motion.span className='absolute bottom-0 right-0 text-2xl'
