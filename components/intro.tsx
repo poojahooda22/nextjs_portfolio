b036757b4d24fad951c3d639130c1b1ceba82899
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-19 21:53:23 +0530

    updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -20,7 +20,8 @@ export default function Intro() {
   
   return (
     <section 
-    ref={ref} id='home' className=' mb-28 max-w-[50rem] text-center 
+    ref={ref} id='home' 
+    className=' mb-28 max-w-[50rem] text-center 
     sm:mb-0 scroll-mt-[100rem]'>
         <div className='flex items-center justify-center'>
             <div className='relative'>
