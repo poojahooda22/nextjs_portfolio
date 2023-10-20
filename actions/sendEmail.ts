42ee339912a1b731d3bf0c51e96a6539bb40ec52
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-20 09:06:57 +0530

    updated

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,47 +1,25 @@
 "use server";
 
-import React from "react";
-// import { Resend } from "resend";
-import { validateString, getErrorMessage } from "@/lib/utils";
-import ContactFormEmail from "@/email/contact-form-email";
+import { Resend } from "resend";
+// import { validateString, getErrorMessage } from "@/lib/utils";
+// import ContactFormEmail from "@/email/contact-form-email";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  // simple server-side validation
-  if (!validateString(senderEmail, 500)) {
+  if(!message) {
     return {
-      error: "Invalid sender email",
-    };
-  }
-  if (!validateString(message, 5000)) {
-    return {
-      error: "Invalid message",
-    };
+      error: "Message is required",
+    }
   }
 
-  let data;
-  try {
-    // data = await resend.emails.send({
-    //   from: "Contact Form <onboarding@resend.dev>",
-    //   to: "bytegrad@gmail.com",
-    //   subject: "Message from contact form",
-    //   reply_to: senderEmail,
-    //   react: React.createElement(ContactFormEmail, {
-    //     message: message,
-    //     senderEmail: senderEmail,
-    //   }),
-    // });
-  } catch (error: unknown) {
-    return {
-      error: getErrorMessage(error),
-    };
-  }
-
-  return {
-    data,
+ resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "poojahooda22@gmail.com",
+      subject: "Message from contact form",
+      text: message,
+    });
   };
-};
