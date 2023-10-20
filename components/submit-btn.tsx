8d09b5273caa606c9f0bb1e5f1ae072a751e92a5
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-20 20:53:22 +0530

    updated

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,28 +1,21 @@
 import React from "react";
 import { FaPaperPlane } from "react-icons/fa";
-// import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
-  // const { pending } = useFormStatus();
 
   return (
-    <button
-      type="submit"
-      className="group flex items-center justify-center 
-      gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none 
-      transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 
-      dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
-      // disabled={pending}
-    >
-      
-        <>
-          Submit{" "}
+    <button 
+          type="submit" 
+          className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] 
+          bg-gray-900 text-white rounded-full outline-none transition-all 
+          dark:bg-[#1d2432]"
+        >
+          Submit {" "}
           <FaPaperPlane 
             className="text-xs opacity-70 transition-all 
-            group-hover:translate-x-1 group-hover:-translate-y-1" 
-          />{" "}
-        </>
-      
+             group-hover:translate-x-1 
+            group-hover:-translate-y-1"  
+           /> 
     </button>
   );
 }
