febf82e8aec20df78bc40fd4456d24b9ed300594
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-20 20:53:02 +0530

    updated

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,27 +1,42 @@
-import { validateString } from './../lib/utils';
-"use server";
 
+"use server";
+import React from "react";
 import { Resend } from "resend";
-// import { validateString, getErrorMessage } from "@/lib/utils";
-// import ContactFormEmail from "@/email/contact-form-email";
+import { getErrorMessage, validateString } from '../lib/utils';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const validateString = (value: unknown) => {
-  if(!value || typeof value !== "string") {
-    return false;
-  }
-}
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
- resend.emails.send({
+  if(!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email"
+    }
+  }
+
+  if(!validateString(message, 5000)) {
+    return {
+      error: "Invalid message"
+    }
+  }
+  let data;
+  try {
+    data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
       to: "poojahooda22@gmail.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
-      text: message,
+      reply_to: senderEmail as string,
+      text: message as string,
     });
+  } catch (error) {
+    return {
+      error: getErrorMessage(error),
+    }
+  }
+  return {
+    data, 
   };
+};
