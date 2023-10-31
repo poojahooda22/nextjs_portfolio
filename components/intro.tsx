8cfe381b7b38925ecd879a7a2dee204a644ad8e7
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-31 20:00:05 +0530

    updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -40,7 +40,8 @@ export default function Intro() {
                         height="192"
                         quality="95"
                         priority={true}
-                        className='h-24 w-24 rounded-full object-cover border-[0.35rem] 
+                        className='h-24 w-24 rounded-full object-cover 
+                        border-[0.35rem] 
                         border-white shadow-xl'
                     />
                 </motion.div>
