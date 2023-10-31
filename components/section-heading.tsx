a3e520efcbbbd36ae41bd22c30c3030cdccc090a
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-31 20:01:46 +0530

    updated section-heading

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -7,7 +7,8 @@ type SectionHeadingProps = {
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
     <h2 
-      className='text-3xl font-medium capitalize mb-8 text-center'>
+      className='text-3xl font-medium capitalize 
+      mb-8 text-center'>
       {children}
     </h2>
   );
