02f01967ddcaf506e90daf6eea1b68ae6e70c3de
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-13 00:29:28 +0530

    updated data

--- lib/data.ts
+++ lib/data.ts
@@ -115,7 +115,7 @@ export const projectsData = [
       "A 3D website with animations and interactions. Used prismic for dynamic content slides.",
     tags: ["React", "Next js", "Typescript", "Three.js", "Prismic", "Framer Motion"],
     imageUrl: website3d,
-    imagePath: "https://disney-clone-eight-azure.vercel.app/"
+    imagePath: "https://pooja-portfolio-one.vercel.app/"
   },
   {
     title: "Portfolio Website",
