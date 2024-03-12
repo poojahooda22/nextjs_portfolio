73b698a406ed52d0b288be38bf3a4f9cf3e500dc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-12 22:53:34 +0530

    updated link

--- components/project.tsx
+++ components/project.tsx
@@ -4,14 +4,14 @@ import React, { useRef } from 'react';
 import { projectsData } from '@/lib/data'
 import Image from 'next/image'
 import { useScroll, useTransform , motion} from 'framer-motion';
-import { SmoothCorners } from 'react-smooth-corners'
+// import { SmoothCorners } from 'react-smooth-corners'
 import Link from 'next/link';
 
 type ProjectProps = (typeof projectsData)[number] & {
     imagePath: string;
 };
  
-export default function Project({ title, description, tags, imageUrl}: 
+export default function Project({ title, description, tags, imageUrl, imagePath}: 
     ProjectProps) {
     const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress} = useScroll ({
@@ -49,26 +49,26 @@ export default function Project({ title, description, tags, imageUrl}:
                 </ul>
             </div> 
             
-                <Image 
-                    src={imageUrl} alt={title} quality={95}
-                    
-                    
-                    className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] object-fit rounded-t-lg shadow-2xl
-                    transition
-                    group-hover:scale-[1.04] 
-                    group-hover:-translate-x-3
-                    group-hover:-translate-y-3
-                    group-hover:rotate-2
+            <a>
+                <Link href={imagePath}>
+                    <Image 
+                        src={imageUrl} alt={title} quality={95} 
+                        className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] object-fit rounded-t-lg shadow-2xl
+                        transition
+                        group-hover:scale-[1.04] 
+                        group-hover:-translate-x-3
+                        group-hover:-translate-y-3
+                        group-hover:rotate-2
 
-                    group-even:group-hover:-translate-x-3
-                    group-even:group-hover:-translate-y-3
-                    group-even:group-hover:rotate-2
+                        group-even:group-hover:-translate-x-3
+                        group-even:group-hover:-translate-y-3
+                        group-even:group-hover:rotate-2
 
-                    group-even:right-[initial] 
-                    group-even:-left-40'
-                />
-            
-            
+                        group-even:right-[initial] 
+                        group-even:-left-40'
+                    />
+                </Link>
+            </a>   
         </section>  
     </motion.div>
     );
