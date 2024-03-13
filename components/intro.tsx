5e28cf93f585ab375fc21a868bd8c94898413b1c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-13 22:51:43 +0530

    updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -86,9 +86,7 @@ export default function Intro() {
                 py-3 flex items-center gap-2 rounded-full
                 outline-none focus:scale-110 hover:scale-110
                 hover:bg-gray-950 active:scale-105
-                transition'
-                
-                                 
+                transition'                   
              */}
             <a
                 className='group bg-white px-7 py-3 flex items-center gap-2 
