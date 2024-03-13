2e5e6ea79403010e1009936db05f2f24cbe29e3c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-13 22:44:03 +0530

    updated intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
                   }}
             >
                 Contact me here <BsArrowRight 
-                className='opacity-70 group-hover:translate-x-1                 
+                className='opacity-70 group-hover:trans                 
              */}
             <a
                 className='group bg-white px-7 py-3 flex items-center gap-2 
