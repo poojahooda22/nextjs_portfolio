873c0a6ee20d03ed48f7b47a93fbd4698ca3c998
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-13 23:02:16 +0530

    updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -81,8 +81,7 @@ export default function Intro() {
             }}
         >
             {/* <Link 
-                href="#contact"
-                c             
+                href="#contact"           
              */}
             <a
                 className='group bg-white px-7 py-3 flex items-center gap-2 
