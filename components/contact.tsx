b6091a4322bc9cbfe2e6c5063aa931651a35aa9d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-15 20:55:49 +0530

    updated contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -17,7 +17,8 @@ export default function Contact() {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] 
+      text-center"
       initial={{
         opacity: 0,
       }}
