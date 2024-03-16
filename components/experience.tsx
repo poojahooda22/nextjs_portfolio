cfb7775df976d1590075f44fa2d0c02aa55cfc77
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-16 07:48:45 +0530

    add

--- components/experience.tsx
+++ components/experience.tsx
@@ -47,7 +47,6 @@ export default function Experience() {
                 theme === "light"
                 ? "0.4rem solid #9ca3af"
                 : "0.4rem solid rgba(255, 255, 255, 0.5)",
-
                       }}
                           date={item.date}
                           icon={item.icon}
