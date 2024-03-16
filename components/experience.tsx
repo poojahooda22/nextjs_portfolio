93ca6eb8b34607147dc32e42d1f08957a4817107
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-16 07:55:04 +0530

    add

--- components/experience.tsx
+++ components/experience.tsx
@@ -62,7 +62,7 @@ export default function Experience() {
                     }}
                     >
                           <h3  className="font-semibold capitalize mb-2">{item.title}</h3>
-                          <p className="font-normal !mt-0  mb-2">{item.location}</p>
+                         <p className="font-normal !mt-0  mb-2">{item.location}</p>
                         <p 
                               className="!mt-1 !font-normal text-gray-700  mb-2 dark:text-white/75"
                             >{item.description}</p>
