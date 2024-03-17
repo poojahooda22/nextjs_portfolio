2fb7ed229be49757eb25348fd42f3fbb968616f1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-17 16:36:58 +0530

    add

--- components/experience.tsx
+++ components/experience.tsx
@@ -68,7 +68,7 @@ export default function Experience() {
                 >
                   {item.description}
                 </p>
-                </ VerticalTimelineElement>
+              </ VerticalTimelineElement>
           </React.Fragment>
         ))}    
       </VerticalTimeline>
