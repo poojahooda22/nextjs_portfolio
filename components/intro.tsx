0553cfab141c64b988fcb95a8e1342a78a1fdc9b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-18 20:39:44 +0530

    add intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -58,7 +58,8 @@ export default function Intro() {
             </div>
         </div>
         <motion.h1 
-            className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] 
+            className='mb-10 mt-4 px-4 text-2xl 
+            font-medium !leading-[1.5] 
             sm:text-4xl'
             initial={{ opacity: 0, y: 100}}
             animate={{ opacity: 1, y: 0}}
