049a2b3ca97cf7c172832cf58160e6013b43376f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-18 20:37:02 +0530

    updated intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -87,7 +87,7 @@ export default function Intro() {
                 Download CV 
                 <HiDownload 
                     className='opacity-70 group-hover:translate-1 
-                transition' />
+                    transition' />
             </a>
             <a 
                 className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105
