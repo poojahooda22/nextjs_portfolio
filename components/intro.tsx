2f6409f75e3a809ab8e33590023ded6aa97d1a35
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-18 20:37:46 +0530

    updated intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,8 @@ export default function Intro() {
             </a>
             <a 
                 className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105
-                transition borderBlack dark:bg-white/10 dark:text-white/60' href='https://www.linkedin.com/in/pooja-hooda-21b473124/' target='_blank'
+                transition borderBlack dark:bg-white/10 dark:text-white/60' 
+                href='https://www.linkedin.com/in/pooja-hooda-21b473124/' target='_blank'
             >
                 <BsLinkedin />
             </a>
