7dc5461dec68561e638a33b2514acb2798a214b9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-18 20:40:32 +0530

    add

--- components/project.tsx
+++ components/project.tsx
@@ -36,7 +36,8 @@ export default function Project({ title, description, tags, imageUrl, imagePath}
             sm:pr-8 relative sm:h-[20rem]
             mb-3 sm:mb-8 last:mb-0 even-pl-8 hover:bg-gray-200 transition rounded-xl
             group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20'>
-            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[20rem]'>
+            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col 
+            h-full group-even:ml-[20rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
                 <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
