2376c7fbe3ad48633a55f8c5b588a98262e6bbcd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-18 20:43:14 +0530

    add

--- components/project.tsx
+++ components/project.tsx
@@ -50,7 +50,8 @@ export default function Project({ title, description, tags, imageUrl, imagePath}
                 <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>
                     {description}
                 </p>
-                <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
+                <ul 
+                className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
                     {tags.map((tag, index) => (
                         <li 
                             className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase 
