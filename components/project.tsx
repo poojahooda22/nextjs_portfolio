62cfb3a7c52ea0a37298e6f6b38f2b6154120b0e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-26 10:20:08 +0530

    added projects

--- components/project.tsx
+++ components/project.tsx
@@ -76,21 +76,21 @@ export default function Project({ title, description, tags, imageUrl, imagePath}
                             ) )}
                         </ul>
                     </div> 
-                        <Image 
-                                src={imageUrl} alt={title} quality={95} 
-                                className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] object-fit 
-                                rounded-t-lg shadow-2xl
-                                transition
-                                group-hover:scale-[1.04] 
-                                group-hover:-translate-x-3
-                                group-hover:-translate-y-3
-                                group-hover:rotate-2
-                                group-even:group-hover:-translate-x-3
-                                group-even:group-hover:-translate-y-3
-                                group-even:group-hover:rotate-2
-                                group-even:right-[initial] 
-                                group-even:-left-40'
-                            /> 
+                    <Image 
+                        src={imageUrl} alt={title} quality={95} 
+                        className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] object-fit 
+                        rounded-t-lg shadow-2xl
+                        transition
+                        group-hover:scale-[1.04] 
+                        group-hover:-translate-x-3
+                        group-hover:-translate-y-3
+                        group-hover:rotate-2
+                        group-even:group-hover:-translate-x-3
+                        group-even:group-hover:-translate-y-3
+                        group-even:group-hover:rotate-2
+                        group-even:right-[initial] 
+                        group-even:-left-40'
+                    /> 
                 </section>  
             </Link>
         </motion.div>
