ebb322b358e14ccdad45ec578a30f7aacb471f88
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-26 10:13:53 +0530

    updated resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -82,7 +82,7 @@ export default function Intro() {
                 rounded-full outline-none focus:scale-110 hover:scale-110
                 hover:bg-white active:scale-105
                 transition cursor-pointer dark:bg-white/10' 
-                href='/Resume_updated.pdf' 
+                href='/Resume.pdf' 
                 download
             >
                 Download CV 
