7321da06c4cc5423c6a8ba06f3d379d226383d7e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-26 10:18:50 +0530

    updated projects

--- components/project.tsx
+++ components/project.tsx
@@ -34,66 +34,66 @@ export default function Project({ title, description, tags, imageUrl, imagePath}
             <Link 
                 href={imagePath}
             >
-        <section 
-            className='group bg-gray-100 max-w-[42rem] border 
-            border-black/5 overflow-hidden 
-            sm:pr-8 relative sm:h-[20rem]
-            mb-3 sm:mb-8 last:mb-0 even-pl-8 hover:bg-gray-200 
-            transition rounded-xl
-            group-even:pl-8 dark:text-white dark:bg-white/10 
-            dark:hover:bg-white/20'
-        >
-            <div 
-                className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 
-                sm:pt-10 
-                sm:max-w-[50%] flex flex-col 
-                h-full group-even:ml-[20rem]'
-            >
-                <h3 
-                    className='text-2xl font-semibold'
+                <section 
+                    className='group bg-gray-100 max-w-[42rem] border 
+                    border-black/5 overflow-hidden 
+                    sm:pr-8 relative sm:h-[20rem]
+                    mb-3 sm:mb-8 last:mb-0 even-pl-8 hover:bg-gray-200 
+                    transition rounded-xl
+                    group-even:pl-8 dark:text-white dark:bg-white/10 
+                    dark:hover:bg-white/20'
                 >
-                    {title}
-                </h3>
-                <p 
-                    className='mt-2 leading-relaxed text-gray-700 
-                    dark:text-white/70'
-                >
-                    {description}
-                </p>
-                <ul 
-                    className='flex flex-wrap mt-4 gap-2 
-                    sm:mt-auto'
-                >
-                    {tags.map((tag, index) => (
-                        <li 
-                            className='bg-black/[0.7] px-3 py-1 text-[0.7rem] 
-                            uppercase 
-                            tracking-wider text-white rounded-full  dark:text-white/70'
-                            key={index}
+                    <div 
+                        className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 
+                        sm:pt-10 
+                        sm:max-w-[50%] flex flex-col 
+                        h-full group-even:ml-[20rem]'
+                    >
+                        <h3 
+                            className='text-2xl font-semibold'
                         >
-                            {tag}
-                        </li>
-                    ) )}
-                </ul>
-            </div> 
-                <Image 
-                        src={imageUrl} alt={title} quality={95} 
-                        className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] object-fit 
-                        rounded-t-lg shadow-2xl
-                        transition
-                        group-hover:scale-[1.04] 
-                        group-hover:-translate-x-3
-                        group-hover:-translate-y-3
-                        group-hover:rotate-2
-                        group-even:group-hover:-translate-x-3
-                        group-even:group-hover:-translate-y-3
-                        group-even:group-hover:rotate-2
-                        group-even:right-[initial] 
-                        group-even:-left-40'
-                    /> 
-        </section>  
-        </Link>
-    </motion.div>
+                            {title}
+                        </h3>
+                        <p 
+                            className='mt-2 leading-relaxed text-gray-700 
+                            dark:text-white/70'
+                        >
+                            {description}
+                        </p>
+                        <ul 
+                            className='flex flex-wrap mt-4 gap-2 
+                            sm:mt-auto'
+                        >
+                            {tags.map((tag, index) => (
+                                <li 
+                                    className='bg-black/[0.7] px-3 py-1 text-[0.7rem] 
+                                    uppercase 
+                                    tracking-wider text-white rounded-full  dark:text-white/70'
+                                    key={index}
+                                >
+                                    {tag}
+                                </li>
+                            ) )}
+                        </ul>
+                    </div> 
+                        <Image 
+                                src={imageUrl} alt={title} quality={95} 
+                                className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] object-fit 
+                                rounded-t-lg shadow-2xl
+                                transition
+                                group-hover:scale-[1.04] 
+                                group-hover:-translate-x-3
+                                group-hover:-translate-y-3
+                                group-hover:rotate-2
+                                group-even:group-hover:-translate-x-3
+                                group-even:group-hover:-translate-y-3
+                                group-even:group-hover:rotate-2
+                                group-even:right-[initial] 
+                                group-even:-left-40'
+                            /> 
+                </section>  
+            </Link>
+        </motion.div>
     );
 }
 
