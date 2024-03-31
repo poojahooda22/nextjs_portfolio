4bc88868b986a1aca3c82eef31cd9744ffcc6161
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-31 11:25:06 +0530

    updated changes

--- lib/data.ts
+++ lib/data.ts
@@ -126,7 +126,7 @@ export const projectsData = [
     title: "Discord Clone",
     description:
       "Integrated PlanetScale and Prisma/client for seamless database management and employed planetscale with mysql.",
-    tags: ["React", "Next js", "Typescript", "Prisma", "Tailwind", "Mysql", "Zustand" ],
+    tags: ["React", "Next js", "Typescript", "Prisma", "Tailwind", "Mysql", "Zustand", "Socket.io" ],
     imageUrl: discord,
     imagePath: "https://discord-clone-git-master-pooja-hoodas-projects.vercel.app/"
   },
