33615891e094b4983d3e650557065616ef020ab4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-17 13:46:03 +0530

    updated contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -37,7 +37,7 @@ export default function Contact() {
       <p className="text-gray-700 -mt-6  dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+          poojahooda22@gmail.com
         </a>{" "}
         or through this form.
       </p>
