72286a26d766bde9441a5dfcd6fcfe120542480f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-23 21:14:12 +0530

    updated form

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,10 +7,12 @@ import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
+import { useRef } from 'react';
 
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+  const formRef = useRef(null);
   
 
   return (
@@ -43,6 +45,7 @@ export default function Contact() {
       </p>
 
       <form
+        ref={formRef}
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
       
@@ -53,6 +56,10 @@ export default function Contact() {
             return;
           }
           toast.success("Email sent successfully!")
+          // Reset the form
+    if (formRef.current) {
+      (formRef.current as HTMLFormElement).reset();
+    }
           
         }}
       >
@@ -65,7 +72,7 @@ export default function Contact() {
           required
           maxLength={500}
           placeholder="Your email"
-          onReset={ }
+          
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-[#f3f4f6] 
