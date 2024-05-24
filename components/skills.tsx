780c2bbcaa6bece237891a153dcc65bda0e95f17
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-24 23:04:57 +0530

    added

--- components/skills.tsx
+++ components/skills.tsx
@@ -39,7 +39,8 @@ export default function Skills() {
         {skillsData.map((skill, index) => (
             <motion.li
                 className="bg-white border border/black[0.1] 
-                rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80 dark:border-none "
+                rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80 
+                dark:border-none "
                 key={index}
                 variants={fadeInAnimationsVariants}
                 initial="initial"
