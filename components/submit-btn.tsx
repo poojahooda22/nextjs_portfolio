d5bd2562fd8b6c359038e3120599fc071122be44
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-25 21:16:27 +0530

    added

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -12,7 +12,7 @@ export default function SubmitBtn() {
     >
       Submit {" "}
       <FaPaperPlane 
-            className="text-xs opacity-70 transition-all 
+          className="text-xs opacity-70 transition-all 
         group-hover:translate-x-1 
         group-hover:-translate-y-1"  
       /> 
