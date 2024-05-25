4aedd9ed16cf76113aa48e176c9ef0b148476edb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-25 21:13:54 +0530

    added

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -8,7 +8,7 @@ export default function SubmitBtn() {
           type="submit" 
           className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] 
           bg-gray-900 text-white rounded-full outline-none transition-all 
-          dark:bg-[#1d2432]"
+        dark:bg-[#1d2432]"
     >
           Submit {" "}
           <FaPaperPlane 
