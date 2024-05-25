968b6e195022c4a15a9baf69b3d4da78cda91d12
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-25 21:15:33 +0530

    added

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -15,7 +15,7 @@ export default function SubmitBtn() {
             className="text-xs opacity-70 transition-all 
              group-hover:translate-x-1 
             group-hover:-translate-y-1"  
-           /> 
+          /> 
     </button>
   );
 }
