a2997191a94b637b29984d65acc50ad413e603be
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-25 21:14:43 +0530

    added

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -6,7 +6,7 @@ export default function SubmitBtn() {
   return (
     <button 
           type="submit" 
-        className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] 
+      className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] 
     bg-gray-900 text-white rounded-full outline-none transition-all 
       dark:bg-[#1d2432]"
     >
