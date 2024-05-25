7c73e08f2f936c5698703d114c3d138fdbc3c191
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-25 21:12:12 +0530

    added

--- components/about.tsx
+++ components/about.tsx
@@ -18,7 +18,7 @@ export default function About() {
       transition={{ delay: 0.175}}
       id='about'
     >
-        <SectionHeading>About me</SectionHeading>
+      <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">Accounting</span>, I decided to pursue my
