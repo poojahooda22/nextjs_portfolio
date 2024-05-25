23638f9a3c76633de8d2aaec54af401200f81bcb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-25 21:11:46 +0530

    added

--- components/about.tsx
+++ components/about.tsx
@@ -13,7 +13,7 @@ export default function About() {
       ref={ref}
       className='mb-28 max-w-[45rem] text-center leading-8 
       sm:mb-40 scroll-mt-28'
-        initial={{ opacity: 0, y: 100}}
+      initial={{ opacity: 0, y: 100}}
         animate={{ opacity: 1, y: 0}}
         transition={{ delay: 0.175}}
         id='about'
