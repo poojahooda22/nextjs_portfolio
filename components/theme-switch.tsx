08a30ef17a58a39b479db791e604881e91e15088
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-26 22:00:15 +0530

    added

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -14,7 +14,7 @@ export default function ThemeSwitch() {
       backdrop-blur-[0.5rem] 
      border-solid borderBlack shadow-xl  overflow-hidden rounded-full flex 
     items-center justify-center hover:scale-[1.15] 
- active:scale-105 
+active:scale-105 
    transition-all  
       dark:bg-gray-950 
       dark:text-gray-50"
