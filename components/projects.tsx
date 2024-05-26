c336ab038c1f07a6a0fa81aebf592a3f4df1cb58
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-26 21:46:52 +0530

    added

--- components/projects.tsx
+++ components/projects.tsx
@@ -29,7 +29,7 @@ export default function Projects() {
                 <React.Fragment key={index}>
                   <Project {...project} />
                 </React.Fragment> 
-            ))}
+          ))}
         </div>
     </section>
   )
