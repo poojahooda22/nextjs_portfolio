0877a6ea45e6d9e8b6d464545c89b7d62ae0e0ee
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-26 21:47:45 +0530

    added

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -7,7 +7,7 @@ export default function SectionDivider() {
   return (
     <motion.div 
       className='bg-gray-200 my-24 h-16 
-        w-1 rounded-full hidden sm:block'
+      w-1 rounded-full hidden sm:block'
         initial={{ opacity: 0, y: 100}}
         animate={{ opacity: 1, y: 0}}
         transition={{ delay: 0.125}}
