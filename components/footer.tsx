3054a69615d3a0528f9cce23539680e2b6055e7c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-25 21:13:31 +0530

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
+      {/* //footer */}
       <small className="mb-2 block text-xs">
         &copy; 2023. All rights reserved.
       </small>
