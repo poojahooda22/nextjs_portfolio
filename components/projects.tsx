39acfb756289d665339f7d4a4c9fd243d1da5c3a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-25 21:43:20 +0530

    added

--- components/projects.tsx
+++ components/projects.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
   return (
     <section ref={ref} id='projects' className='scroll-mt-28 mb-28'>
       <SectionHeading>My Projects</SectionHeading>
-        <div>
+      <div>
           {projectsData.map((project, index) => (
             <React.Fragment key={index}>
               <Project {...project} />
